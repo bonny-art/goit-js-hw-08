ba102ebd97823374ece2605742ad05a82e72dd8a
bonny-art <svitlana.otenko@gmail.com>
2023-10-10 01:29:58 +0300

    makes task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,16 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { createMarkup } from './helpers-gallery';
+
+const container = document.querySelector('.gallery');
+
+container.innerHTML = createMarkup(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
