2d199a8724c41d147a4e6c20ef6a27ee2b630ede
bonny-art <svitlana.otenko@gmail.com>
2023-10-11 02:27:16 +0300

    makes task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,20 @@
-// В завданні 2 домашньої роботи уважно читаємо ТЗ,
-// щоб правильно використати потрібні методи бібліотеки,
-// а саме метод on() з подією timeupdate і метод
-// setCurrentTime() для встановлення часу після
-// перезавантаження сторінки.
-
-// Також не забуваємо перевіряти
-// наявність данних, коли читаєте з localStorage. Якщо
-// в localStorage немає ключа, який ви намагаєтесь прочитати,
-// метод getItem(key) поверне вам null.
-
-import Vimeo from '@vimeo/player';
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (currentTime) {
-  console.log(currentTime);
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
-});
+player.on(
+  'timeupdate',
+  throttle(currentTime => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime)
+    );
+  }, 1000)
+);
 
-const currentTime = JSON.parse(
-  localStorage.getItem('videoplayer-current-time')
-).seconds;
-
-console.log(currentTime);
+const currentTime =
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))?.seconds ?? 0;
 
 player.setCurrentTime(currentTime);
