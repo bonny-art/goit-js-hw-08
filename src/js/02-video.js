2a495c1822e6766ac1ebea3d993ccdd6d4477985
bonny-art <svitlana.otenko@gmail.com>
2023-10-11 00:59:21 +0300

    makes resumption of playback from a saved position

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// В завданні 2 домашньої роботи уважно читаємо ТЗ,
+// щоб правильно використати потрібні методи бібліотеки,
+// а саме метод on() з подією timeupdate і метод
+// setCurrentTime() для встановлення часу після
+// перезавантаження сторінки.
 
+// Також не забуваємо перевіряти
+// наявність данних, коли читаєте з localStorage. Якщо
+// в localStorage немає ключа, який ви намагаєтесь прочитати,
+// метод getItem(key) поверне вам null.
+
+import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (currentTime) {
+  console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+});
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+).seconds;
+
+console.log(currentTime);
+
+player.setCurrentTime(currentTime);
